1998ab9f5f6c5119055b23aed6c50f167b33bb37
Ognjen <ognjen@heliozgroup.com>
2017-08-04 08:40:43 +0200

    add browser example

---
+++ examples/browser/dxf_bundle.js
@@ -0,0 +1,398 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"/src/Arc.js":[function(require,module,exports){
+class Arc
+{
+    /**
+     * @param {number} x1 - Center x
+     * @param {number} y1 - Center y
+     * @param {number} r - radius
+     * @param {number} startAngle - degree 
+     * @param {number} endAngle - degree 
+     */
+    constructor(x1, y1, r, startAngle, endAngle)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.r = r;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+    }
+
+    toDxfString()
+    {
+        //https://www.autodesk.com/techpubs/autocad/acadr14/dxf/line_al_u05_c.htm
+        let s = `0\nARC\n`;
+        s += `10\n${this.x1}\n20\n${this.y1}\n30\n0\n`;
+        s += `40\n${this.r}\n50\n${this.startAngle}\n51\n${this.endAngle}\n`;
+        return s;
+    }
+}
+
+module.exports = Arc;
+},{}],"/src/Circle.js":[function(require,module,exports){
+class Circle
+{
+    /**
+     * @param {number} x1 - Center x
+     * @param {number} y1 - Center y
+     * @param {number} r - radius
+     */
+    constructor(x1, y1, r)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.r = r;
+    }
+
+    toDxfString()
+    {
+        //https://www.autodesk.com/techpubs/autocad/acadr14/dxf/line_al_u05_c.htm
+        let s = `0\nCIRCLE\n`;
+        s += `10\n${this.x1}\n20\n${this.y1}\n30\n0\n`;
+        s += `40\n${this.r}\n`;
+        return s;
+    }
+}
+
+module.exports = Circle;
+},{}],"/src/Layer.js":[function(require,module,exports){
+class Layer
+{
+    constructor(name, colorNumber, lineTypeName)
+    {
+        this.name = name;
+        this.colorNumber = colorNumber;
+        this.lineTypeName = lineTypeName;
+        this.shapes = [];
+    }
+
+    toDxfString()
+    {
+        let s = '0\nLAYER\n';
+        s += `2\n${this.name}\n`;
+        s += `62\n${this.colorNumber}\n`;
+        s += `6\n${this.lineTypeName}\n`;
+        return s;        
+    }
+
+    addShape(shape)
+    {
+        this.shapes.push(shape);
+    }
+
+    getShapes()
+    {
+        return this.shapes;
+    }
+
+    shapesToDxf()
+    {
+        let s = '';
+        for (let i = 0; i < this.shapes.length; ++i)
+        {
+            s += this.shapes[i].toDxfString();
+            s += `8\n${this.name}\n`;
+        } 
+        
+        
+        return s;
+    }
+}
+
+module.exports = Layer;
+},{}],"/src/Line.js":[function(require,module,exports){
+class Line
+{
+    constructor(x1, y1, x2, y2)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    toDxfString()
+    {
+        //https://www.autodesk.com/techpubs/autocad/acadr14/dxf/line_al_u05_c.htm
+        let s = `0\nLINE\n`;
+        s += `10\n${this.x1}\n20\n${this.y1}\n30\n0\n`;
+        s += `11\n${this.x2}\n21\n${this.y2}\n31\n0\n`;
+        return s;
+    }
+}
+
+module.exports = Line;
+},{}],"/src/Text.js":[function(require,module,exports){
+class Text
+{
+    /**
+     * @param {number} x1 - x
+     * @param {number} y1 - y
+     * @param {number} height - Text height
+     * @param {number} rotation - Text rotation
+     * @param {string} value - the string itself
+     */
+    constructor(x1, y1, height, rotation, value)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.height = height;
+        this.rotation = rotation;
+        this.value = value;
+    }
+
+    toDxfString()
+    {
+        //https://www.autodesk.com/techpubs/autocad/acadr14/dxf/line_al_u05_c.htm
+        let s = `0\nTEXT\n`;
+        s += `1\n${this.value}\n`;
+        s += `10\n${this.x1}\n20\n${this.y1}\n30\n0\n`;
+        s += `40\n${this.height}\n50\n${this.rotation}\n`;
+        return s;
+    }
+}
+
+module.exports = Text;
+},{}],1:[function(require,module,exports){
+class LineType
+{
+    /**
+     * @param {string} name
+     * @param {string} description
+     * @param {array} elements - if elem > 0 it is a line, if elem < 0 it is gap, if elem == 0.0 it is a 
+     */
+    constructor(name, description, elements)
+    {
+        this.name = name;
+        this.description = description;
+        this.elements = elements;
+    }
+
+    /**
+     * @link https://www.autodesk.com/techpubs/autocad/acadr14/dxf/ltype_al_u05_c.htm
+     */
+    toDxfString()
+    {
+        let s = '0\nLTYPE\n';
+        s += '72\n65\n';
+        s += `2\n${this.name}\n`;
+        s += `3\n${this.description}\n`;
+        s += `73\n${this.elements.length}\n`;
+        s += `40\n${this.getElementsSum()}\n`;
+
+        for (let i = 0; i < this.elements.length; ++i)
+        {
+            s += `49\n${this.elements[i]}\n`;
+        }
+
+        return s;
+    }
+
+    getElementsSum()
+    {
+        let sum = 0;
+        for (let i = 0; i < this.elements.length; ++i)
+        {
+            sum += Math.abs(this.elements[i]);
+        }
+
+        return sum;
+    }
+}
+
+module.exports = LineType;
+},{}],"Drawing":[function(require,module,exports){
+const LineType = require('./LineType');
+const Layer = require('./Layer');
+const Line = require('./Line');
+const Arc = require('./Arc');
+const Circle = require('./Circle');
+const Text = require('./Text');
+
+class Drawing
+{
+    constructor()
+    {
+        this.layers = {};
+        this.activeLayer = null;
+        this.lineTypes = {};
+
+        for (let i = 0; i < Drawing.LINE_TYPES.length; ++i)
+        {
+            this.addLineType(Drawing.LINE_TYPES[i].name,
+                             Drawing.LINE_TYPES[i].description,
+                             Drawing.LINE_TYPES[i].elements);
+        }
+
+        for (let i = 0; i < Drawing.LAYERS.length; ++i)
+        {
+            this.addLayer(Drawing.LAYERS[i].name,
+                          Drawing.LAYERS[i].colorNumber,
+                          Drawing.LAYERS[i].lineTypeName);
+        }
+
+        this.setActiveLayer('0');
+    }
+    
+    
+    /**
+     * @param {string} name
+     * @param {string} description
+     * @param {array} elements - if elem > 0 it is a line, if elem < 0 it is gap, if elem == 0.0 it is a 
+     */
+    addLineType(name, description, elements)
+    {
+        this.lineTypes[name] = new LineType(name, description, elements);
+        return this;
+    }
+
+    addLayer(name, colorNumber, lineTypeName)
+    {
+        this.layers[name] = new Layer(name, colorNumber, lineTypeName);
+        return this;
+    }
+    
+    setActiveLayer(name)
+    {
+        this.activeLayer = this.layers[name];
+        return this;
+    }
+
+    drawLine(x1, y1, x2, y2)
+    {
+        this.activeLayer.addShape(new Line(x1, y1, x2, y2));
+        return this;
+    }
+
+    /**
+     * @param {number} x1 - Center x
+     * @param {number} y1 - Center y
+     * @param {number} r - radius
+     * @param {number} startAngle - degree 
+     * @param {number} endAngle - degree 
+     */
+    drawArc(x1, y1, r, startAngle, endAngle)
+    {
+        this.activeLayer.addShape(new Arc(x1, y1, r, startAngle, endAngle));
+        return this;
+    }
+
+    /**
+     * @param {number} x1 - Center x
+     * @param {number} y1 - Center y
+     * @param {number} r - radius
+     */
+    drawCircle(x1, y1, r)
+    {
+        this.activeLayer.addShape(new Circle(x1, y1, r));
+        return this;
+    }
+
+    /**
+     * @param {number} x1 - x
+     * @param {number} y1 - y
+     * @param {number} height - Text height
+     * @param {number} rotation - Text rotation
+     * @param {string} value - the string itself
+     */
+    drawText(x1, y1, height, rotation, value)
+    {
+        this.activeLayer.addShape(new Text(x1, y1, height, rotation, value));
+        return this;
+    }
+
+    _getDxfLtypeTable()
+    {
+        let s = '0\nTABLE\n'; //start table
+        s += '2\nLTYPE\n';    //name table as LTYPE table
+
+        for (let lineTypeName in this.lineTypes)
+        {
+            s += this.lineTypes[lineTypeName].toDxfString();
+        }
+
+        s += '0\nENDTAB\n'; //end table
+
+        return s;
+    }
+
+    _getDxfLayerTable()
+    {
+        let s = '0\nTABLE\n'; //start table
+        s += '2\nLAYER\n'; //name table as LAYER table
+
+        for (let layerName in this.layers)
+        {
+            s += this.layers[layerName].toDxfString();
+        }
+
+        s += '0\nENDTAB\n';
+
+        return s;
+    }
+
+    toDxfString()
+    {
+        let s = '';
+
+        //start section
+        s += '0\nSECTION\n';
+        //name section as TABLES section
+        s += '2\nTABLES\n';
+
+        s += this._getDxfLtypeTable();
+        s += this._getDxfLayerTable();
+
+        //end section
+        s += '0\nENDSEC\n';
+
+
+        //ENTITES section
+        s += '0\nSECTION\n';
+        s += '2\nENTITIES\n';
+
+        for (let layerName in this.layers)
+        {
+            let layer = this.layers[layerName];
+            s += layer.shapesToDxf();
+            // let shapes = layer.getShapes();
+        }
+
+        s += '0\nENDSEC\n';
+
+
+        //close file
+        s += '0\nEOF';
+
+        return s;
+    }
+
+}
+
+//AutoCAD Color Index (ACI)
+//http://sub-atomic.com/~moses/acadcolors.html
+Drawing.ACI = 
+{
+    LAYER : 0,
+    RED : 1,
+    YELLOW : 2,
+    GREEN : 3,
+    CYAN : 4,
+    BLUE : 5,
+    MAGENTA : 6,
+    WHITE : 7
+}
+
+Drawing.LINE_TYPES = 
+[
+    {name: 'CONTINOUS', description: '______', elements: []},
+    {name: 'DASHED',    description: '_ _ _ ', elements: [0.5, -0.5]},
+    {name: 'DOTTED',    description: '. . . ', elements: [0.0, -0.5]}
+]
+
+Drawing.LAYERS = 
+[
+    {name: '0',  colorNumber: Drawing.ACI.WHITE, lineTypeName: 'CONTINOUS'}
+]
+
+module.exports = Drawing;
+},{"./Arc":"/src/Arc.js","./Circle":"/src/Circle.js","./Layer":"/src/Layer.js","./Line":"/src/Line.js","./LineType":1,"./Text":"/src/Text.js"}]},{},[]);
