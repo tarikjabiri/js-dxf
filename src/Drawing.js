b2c3168f0bfeb081aa787450a31185de0479ccee
Carlos Manuel Pereira Lopes <cmplopes67@gmail.com>
2018-04-24 22:41:54 +0100

    Update Drawing.js
    Added method drawRect

--- src/Drawing.js
+++ src/Drawing.js
@@ -61,6 +61,15 @@ class Drawing
         return this;
     }
 
+    drawRect(x1, y1, x2, y2)
+    {
+        this.activeLayer.addShape(new Line(x1, y1, x2, y1));
+        this.activeLayer.addShape(new Line(x1, y2, x2, y2));
+        this.activeLayer.addShape(new Line(x1, y1, x1, y2));
+        this.activeLayer.addShape(new Line(x2, y1, x2, y2));
+        return this;
+    }
+
     /**
      * @param {number} x1 - Center x
      * @param {number} y1 - Center y
