7b04d18fef385fb179c32a5fd4a463c16c663c6a
Duncan Stewart <dunkstewart@gmail.com>
2018-09-10 09:06:06 +1000

    Viewport fix

--- src/Viewport.js
+++ src/Viewport.js
@@ -1,23 +1,22 @@
 class Viewport
 {
-    constructor(centerX, centerY, centerZ, width, height, status)
+    constructor(centerX, centerY, width, height)
     {
         this.centerX = centerX;
         this.centerY = centerY;
-        this.centerZ = centerZ;
         this.width = width;
         this.height = height;
-        this.status = status;
     }
 
     toDxfString()
     {
         //https://www.autodesk.com/techpubs/autocad/acadr14/dxf/viewport_al_u05_c.htm
         let s = `0\nVIEWPORT\n`;
-        s += `10\n${this.centerX}\n20\n${this.centerY}\n30\n${this.centerZ}\n`;
+        s += `10\n${this.centerX}\n20\n${this.centerY}\n`;
         s += `40\n${this.width}\n`;
         s += `41\n${this.height}\n`;
-        s += `68\n${this.status}\n`;
+        s += `68\n1\n`;
+        s += `69\n1\n`;
         return s;
     }
 }
