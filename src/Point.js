aae456e33f1525aa19962ffed227a3a041876387
Ognjen <ognjen@heliozgroup.com>
2019-08-11 16:00:16 +0200

    Fix ponit

--- src/Point.js
+++ src/Point.js
@@ -9,7 +9,7 @@ class Point
     toDxfString()
     {
         //https://www.autodesk.com/techpubs/autocad/acadr14/dxf/point_al_u05_c.htm
-        let s = `0\nLINE\n`;
+        let s = `0\nPOINT\n`;
         s += `8\n${this.layer.name}\n`;
         s += `10\n${this.x}\n20\n${this.y}\n30\n0\n`;
         return s;
