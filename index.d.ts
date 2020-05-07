a11187be4cac67bd62c5aeab7a58ec6b3cd4b410
Ian Overgard <>
2020-05-07 16:08:43 -0600

    Fix some issues with typescript declarations

--- index.d.ts
+++ index.d.ts
@@ -36,6 +36,13 @@ declare module "dxf-writer" {
     }
 
     export class Arc implements RenderableToDxf {
+
+        public x1: number;
+        public y1: number;
+        public r: number;
+        public startAngle: number;
+        public endAngle: number;
+
         /**
          * @param {number} x1 - Center x
          * @param {number} y1 - Center y
@@ -44,45 +51,70 @@ declare module "dxf-writer" {
          * @param {number} endAngle - degree
          */
         constructor(
-            public x1: number,
-            public y1: number,
-            public r: number,
-            public startAngle: number,
-            public endAngle: number,
+            x1: number,
+            y1: number,
+            r: number,
+            startAngle: number,
+            endAngle: number,
         );
+
+        toDxfString(): string;
     }
 
     export class Circle implements RenderableToDxf {
+        public x1: number;
+        public y1: number;
+        public r: number;
+
         /**
          * @param {number} x1 - Center x
          * @param {number} y1 - Center y
          * @param {number} r - radius
          */
         constructor(x1: number, y1: number, r: number);
+        toDxfString(): string;
     }
 
     export class Face implements RenderableToDxf {
+        public x1: number;
+        public y1: number;
+        public z1: number;
+        public x2: number;
+        public y2: number;
+        public z2: number;
+        public x3: number;
+        public y3: number;
+        public z3: number;
+        public x4: number;
+        public y4: number;
+        public z4: number;
+
         constructor(
-            public x1: number,
-            public y1: number,
-            public z1: number,
-            public x2: number,
-            public y2: number,
-            public z2: number,
-            public x3: number,
-            public y3: number,
-            public z3: number,
-            public x4: number,
-            public y4: number,
-            public z4: number,
+            x1: number,
+            y1: number,
+            z1: number,
+            x2: number,
+            y2: number,
+            z2: number,
+            x3: number,
+            y3: number,
+            z3: number,
+            x4: number,
+            y4: number,
+            z4: number,
         );
+        toDxfString(): string;
     }
 
     export class Layer implements RenderableToDxf {
-        constructor(public name: string, public colorNumber: number, public lineTypeName: string);
+        public name: string;
+        public colorNumber: number;
+        public lineTypeName: string;
+        public shapes: RenderableToDxf[];
+        public trueColor: number;
 
-        shapes: RenderableToDxf[];
-        trueColor: number;
+        constructor(name: string, colorNumber: number, lineTypeName: string);
+        toDxfString(): string;
 
         setTrueColor(color: number);
         addShape(shape: RenderableToDxf);
@@ -91,34 +123,61 @@ declare module "dxf-writer" {
     }
 
     export class Line implements RenderableToDxf {
-        constructor(public x1: number, public y1: number, public x2: number, public y2: number);
+        public x1: number;
+        public y1: number;
+        public x2: number; 
+        public y2: number;
+
+        constructor(x1: number, y1: number, x2: number, y2: number);
+        toDxfString(): string;
     }
 
     export class LineType implements RenderableToDxf {
+        public name: string; 
+        public description: string; 
+        public elements: Array<number>;
+
         /**
          * @param {string} name
          * @param {string} description
          * @param {array} elements - if elem > 0 it is a line, if elem < 0 it is gap, if elem == 0.0 it is a
          */
-        constructor(public name: string, public description: string, public elements: Array<number>);
+        constructor(name: string, description: string, elements: Array<number>);
 
         toDxfString(): string;
         getElementsSum(): number;
     }
 
     export class Point implements RenderableToDxf {
-        constructor(public x: number, public y: number);
+        public x: number;
+        public y: number;
+        
+        constructor(x: number, y: number);
+        toDxfString(): string;
     }
 
     export class Polyline implements RenderableToDxf {
-        constructor(public points: Array<Point2D>);
+        public points: Array<Point2D>;
+
+        constructor(points: Array<Point2D>);
+        toDxfString(): string;
     }
 
     export class Polyline3D implements RenderableToDxf {
-        constructor(public points: Array<Point3D>);
+        public points: Array<Point3D>;
+
+        constructor(points: Array<Point3D>);
+        toDxfString(): string;
     }
 
     export class Text implements RenderableToDxf {
+        public x1: number;
+        public y1: number;
+        public height: number;
+        public rotation: number;
+        public value: string;
+        public horizontalAlignment: HorizontalAlignment;
+        public verticalAlignment: VerticalAlignment;
         /**
          * @param {number} x1 - x
          * @param {number} y1 - y
@@ -129,23 +188,34 @@ declare module "dxf-writer" {
          * @param {VerticalAlignment} [verticalAlignment="baseline"] baseline | bottom | middle | top
          */
         constructor(
-            public x1: number,
-            public y1: number,
-            public height: number,
-            public rotation: number,
-            public value: string,
-            public horizontalAlignment: HorizontalAlignment = "left",
-            public verticalAlignment: VerticalAlignment = "baseline",
+            x1: number,
+            y1: number,
+            height: number,
+            rotation: number,
+            value: string,
+            horizontalAlignment?: HorizontalAlignment,
+            verticalAlignment?: VerticalAlignment
         );
+        toDxfString(): string;
     }
+
+    export type ACIKey = 'LAYER'
+        | 'RED'
+        | 'YELLOW'
+        | 'GREEN'
+        | 'CYAN'
+        | 'BLUE'
+        | 'MAGENTA'
+        | 'WHITE'
+        ;
 
     export default class Drawing implements RenderableToDxf {
         constructor();
 
-        layers: { [string]: Layer };
+        layers: { [key: string]: Layer };
         activeLayer: Layer | null;
-        lineTypes: { [string]: LineType };
-        headers: { [string]: Array<HeaderValue> };
+        lineTypes: { [key: string]: LineType };
+        headers: { [key: string]: Array<HeaderValue> };
 
         /**
          * @param {string} name
@@ -191,8 +261,8 @@ declare module "dxf-writer" {
             height: number,
             rotation: number,
             value: string,
-            horizontalAlignment: HorizontalAlignment = "left",
-            verticalAlignment: VerticalAlignment = "baseline",
+            horizontalAlignment?: HorizontalAlignment,
+            verticalAlignment?: VerticalAlignment,
         ): Drawing;
 
         /**
@@ -249,50 +319,15 @@ declare module "dxf-writer" {
          * AutoCAD Color Index (ACI)
          * @see http://sub-atomic.com/~moses/acadcolors.html
          */
-        static ACI = {
-            LAYER: 0,
-            RED: 1,
-            YELLOW: 2,
-            GREEN: 3,
-            CYAN: 4,
-            BLUE: 5,
-            MAGENTA: 6,
-            WHITE: 7,
-        };
+        static ACI: { [key in ACIKey]: number};
 
-        static LINE_TYPES: LineType[] = [
-            { name: "CONTINUOUS", description: "______", elements: [] },
-            { name: "DASHED", description: "_ _ _ ", elements: [5.0, -5.0] },
-            { name: "DOTTED", description: ". . . ", elements: [0.0, -5.0] },
-        ];
+        static LINE_TYPES: LineType[];
 
-        static LAYERS: Layer[] = [{ name: "0", colorNumber: Drawing.ACI.WHITE, lineTypeName: "CONTINUOUS" }];
+        static LAYERS: Layer[];
 
         /**
          * @see https://www.autodesk.com/techpubs/autocad/acad2000/dxf/header_section_group_codes_dxf_02.htm
          */
-        static UNITS: { [Unit]: number } = {
-            Unitless: 0,
-            Inches: 1,
-            Feet: 2,
-            Miles: 3,
-            Millimeters: 4,
-            Centimeters: 5,
-            Meters: 6,
-            Kilometers: 7,
-            Microinches: 8,
-            Mils: 9,
-            Yards: 10,
-            Angstroms: 11,
-            Nanometers: 12,
-            Microns: 13,
-            Decimeters: 14,
-            Decameters: 15,
-            Hectometers: 16,
-            Gigameters: 17,
-            "Astronomical units": 18,
-            "Light years": 19,
-            Parsecs: 20,
-        };
+        static UNITS: { [key in Unit]: number };
     }
 }
